f9b6793fa1ba0172ce3bc414a1c4d8fddd197264
Adam <73717269+adamabad@users.noreply.github.com>
2023-06-15 16:45:40 -0400

    Project Upload
    Main local branch upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './utilities/_variables.scss'
+import './App.scss';
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './components/Home';
+import About from './components/About';
+import useLocalStorage from 'use-local-storage';
+
+function App() {
+    const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+
+    const switchTheme = () => {
+        const newTheme = theme === 'light' ? 'dark' : 'light';
+        setTheme(newTheme);
+    }
+
+    return (
+        <div className="app" data-theme={'light'}>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path='about' element={<About />} />
+                </Route>
+            </Routes>
+        </div>
+    );
+}
+
+export default App;
